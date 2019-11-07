add7ffec7733689d2eb1719089936c061a612208
Anest <anest@66ws.cc>
2019-11-07 00:34:44 -0400

    update and fix Init sqlite Database file

--- Tools/GetInitDatabaseSqlite3.py
+++ Tools/GetInitDatabaseSqlite3.py
@@ -1,3 +1,4 @@
+# Database Version: 4.1
 import os.path
 import sqlite3
 if not os.path.exists("Database.db"):
@@ -8,14 +9,13 @@ if not os.path.exists("Database.db"):
     c.execute('CREATE TABLE User (Account CLOB,Password CLOB);')
     c.execute('CREATE TABLE Note (Note1 CLOB,Note2 CLOB);')
     conn.commit()
-    c.execute("insert into Note values('Version','4.0')")
-    c.execute("insert into User values('admin','admin')")
-    c.execute('insert into Data values("dGVzdC5jb20=","dGVzdA==","MTIzNDU2","MTk3MC0wMS0wMS0tMDAtMDAtMDAtLVRodXJzZGF5","SW5pdGlhbCBUZXN0aW5n")')
+    c.execute("insert into Note values('Version','4.0');")
+    c.execute("insert into User values('admin','admin');")
+    c.execute('insert into Data values("dGVzdC5jb20=","dGVzdA==","MTIzNDU2","MTk3MC0wMS0wMS0tMDAtMDAtMDAtLVRodXJzZGF5","initial Testing");')
     # dGVzdC5jb20= test.com
     # dGVzdA== test
     # MTIzNDU2 123456
     # MTk3MC0wMS0wMS0tMDAtMDAtMDAtLVRodXJzZGF5 1970-01-01--00-00-00--Thursday
-    # SW5pdGlhbCBUZXN0aW5n initial Testing
     conn.commit()
     conn.close()
     del conn, c
