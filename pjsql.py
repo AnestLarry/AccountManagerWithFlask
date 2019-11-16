e1ac90fe92522cf5ad381e8053f96c7923a9ef3f
Anest <anest@66ws.cc>
2019-11-16 10:04:50 -0400

    Optimize SQL

--- pjsql.py
+++ pjsql.py
@@ -18,25 +18,26 @@ class manage_sql:
         self.__c = self.__conn.cursor()
 
     def Save_Result_to_sql(self, AddressStr: str, AccountStr: str, password: str, Text: str) -> bool:
-        self.__c.execute('insert into Data values("'+AddressStr+'","'+AccountStr + '","' + password + '","' +
-                         base64.b64encode(time.strftime(r"%Y-%m-%d--%H-%M-%S--%A").encode()).decode()+'","'+Text+'");')
+        self.__c.execute('insert into Data values(?,?,?,?,?);', (AddressStr, AccountStr,
+                                                                 password, base64.b64encode(time.strftime(r"%Y-%m-%d--%H-%M-%S--%A").encode()).decode(), Text))
         self.__conn.commit()
         return True
 
     def Search_Item(self, key: str, keywordStr: str, language: str = "en-us") -> str:
         if key == "Text":
             self.__c.execute(
-                'select Address,Account,Password,Date,Text from Data where Text LIKE "%'+keywordStr+'%";')
+                r"select Address,Account,Password,Date,Text from Data where Text LIKE ?;", ("%{}%".format(keywordStr),))
         else:
             self.__c.execute(
-                'select Address,Account,Password,Date,Text from Data where "'+key+'" = "'+keywordStr+'";')
+                'select Address,Account,Password,Date,Text from Data where {} = ?;'.format(key), (keywordStr,))
         result_Str: str = '<font size=5>'
         for Item in self.__c.fetchall():
-            result_Str += '<table border="1"><tr><td>'+self.__Search_Item_translate_tag(language, "Address")+'</td><td><input readonly style="width:250px" onfocus="this.select()" value="'+base64.b64decode(Item[0].encode()).decode()+'"/></td></tr> \
-            <tr><td>  '+self.__Search_Item_translate_tag(language, "Account")+' </td><td><input readonly style="width:250px" onfocus="this.select()" value="'+base64.b64decode(Item[1].encode()).decode()+'"/></td></tr> \
-            <tr><td>'+self.__Search_Item_translate_tag(language, "Password")+' </td><td><input readonly style="width:250px" onfocus="this.select()" value="'+base64.b64decode(Item[2].encode()).decode()+'"/></td></tr>\
-            <tr><td>  '+self.__Search_Item_translate_tag(language, "Date")+'  </td><td><input readonly style="width:250px" onfocus="this.select()" value="'+base64.b64decode(Item[3].encode()).decode()+'"/></td></tr>\
-            <tr><td>  '+self.__Search_Item_translate_tag(language, "Text")+'  </td><td><input readonly style="width:250px" onfocus="this.select()" value="'+Item[4] + '"/></td></tr></table><br>'
+            result_Str += '<table border="1"><tr><td>{}</td><td><input readonly style="width:250px" onfocus="this.select()" value="{}"/></td></tr> \
+            <tr><td>  {} </td><td><input readonly style="width:250px" onfocus="this.select()" value="{}"/></td></tr> \
+            <tr><td>{} </td><td><input readonly style="width:250px" onfocus="this.select()" value="{}"/></td></tr>\
+            <tr><td>  {}  </td><td><input readonly style="width:250px" onfocus="this.select()" value="{}"/></td></tr>\
+            <tr><td>  {}  </td><td><input readonly style="width:250px" onfocus="this.select()" value="{}"/></td></tr></table><br>'.format(self.__Search_Item_translate_tag(language, "Address"), base64.b64decode(Item[0].encode()).decode(), self.__Search_Item_translate_tag(language, "Account"), base64.b64decode(Item[1].encode()).decode(), self.__Search_Item_translate_tag(language, "Password"), base64.b64decode(Item[2].encode()).decode(), self.__Search_Item_translate_tag(language, "Date"), base64.b64decode(Item[3].encode()).decode(), self.__Search_Item_translate_tag(language, "Text"), Item[4])
+
         result_Str += '</font>'
         return result_Str
 
@@ -56,19 +57,17 @@ class manage_sql:
             return string
 
     def Update_Text(self, DateStr: str, TextStr: str) -> bool:
-        self.__c.execute('update Data set Text="'+TextStr +
-                         '" where Date="'+DateStr + '";')
+        self.__c.execute('update Data set Text=? where Date=?;',(TextStr,DateStr,))
         self.__conn.commit()
         return True
 
     def Delete_Item(self, keywordStr: str) -> str:
         self.__c.execute(
-            'select Address,Account,Password,Date,Text from Data where "Date" = "'+keywordStr+'";')
+            'select Address,Account,Password,Date,Text from Data where Date = ?;',(keywordStr,))
         __result: str = ""
         for Item in self.__c.fetchall():
-            __result += "Address { "+Item[0]+" } Account { " + \
-                Item[1]+"} Password {"+Item[2]+"} Text {"+Item[4]+" }"
-        self.__c.execute('delete from Data where Date = "'+keywordStr+'";')
+            __result += "Address [ {} ] Account [ {} ] Password [ {} ] Text [ {} ]".format(Item[0],Item[1],Item[2],Item[4])
+        self.__c.execute('delete from Data where Date = ?;',(keywordStr,))
         self.__conn.commit()
         return __result
 
